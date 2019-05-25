c1ab7acc5b4e7d51ba55df925362399ad82fc329
Sergii Zazymko <sergiy.zazymko@gns-it.com>
2019-05-25 11:43:04 +0300

    Add docs and tests

---
+++ tests/SergiiZazymkoTest/Paginator/DirPaginatorTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sergii
+ * Date: 25.05.19
+ * Time: 11:33
+ */
+
+namespace SergiiZazymkoTest\Paginator;
+
+use PHPUnit\Framework\TestCase;
+use SergiiZazymko\Paginator\DirPaginator;
+use SergiiZazymko\Paginator\DirView;
+use SergiiZazymko\Paginator\Paginator;
+
+/**
+ * Class DirPaginatorTest
+ * @package SergiiZazymkoTest\Paginator
+ */
+class DirPaginatorTest extends TestCase
+{
+    /** @var DirPaginator$instance */
+    private $instance;
+
+    protected function setUp(): void
+    {
+        $this->instance = new DirPaginator(new DirView);
+        parent::setUp();
+    }
+
+    public function testInstance()
+    {
+        $this->assertInstanceOf(Paginator::class, $this->instance);
+        $this->assertInstanceOf(DirPaginator::class, $this->instance);
+    }
+}
